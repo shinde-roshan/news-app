3ccb0b5c989c41f0930c6ee9577d9e24ab625b96
roshan <roshan.shinde164@gmail.com>
2025-01-11 16:48:34 +0530

    news model changed according to api response

--- app/src/main/java/com/example/news/data/model/News.kt
+++ app/src/main/java/com/example/news/data/model/News.kt
@@ -1,9 +1,14 @@
 package com.example.news.data.model
 
+import com.squareup.moshi.Json
+
 data class News(
-    val id: Int?,
-    val date: String?,
+    val source: NewsSource?,
     val title: String?,
+    val author: String?,
+    val description: String?,
     val content: String?,
-    val header_img: String?
+    val url: String?,
+    @Json(name = "urlToImage") val headerImgUrl: String?,
+    val publishedAt: String?
 )
---
+++ app/src/main/java/com/example/news/data/model/NewsSource.kt
@@ -0,0 +1,6 @@
+package com.example.news.data.model
+
+data class NewsSource(
+    val id: String?,
+    val name: String?
+)

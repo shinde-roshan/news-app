45d65b25a5b82b2e5fc9c73a68cb39786441419f
roshan <roshan.shinde164@gmail.com>
2025-01-11 11:09:44 +0530

    news data model and sample repository created

---
+++ app/src/main/java/com/example/news/data/model/News.kt
@@ -0,0 +1,9 @@
+package com.example.news.data.model
+
+data class News(
+    val id: Int?,
+    val date: String?,
+    val title: String?,
+    val content: String?,
+    val header_img: String?
+)

f2c5e2f1abffde2c15eb81ff7edae07e200d0ebf
roshan <roshan.shinde164@gmail.com>
2025-01-12 12:06:26 +0530

    opening the news url in browser on clicking news item

--- app/src/main/java/com/example/news/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/news/ui/home/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.example.news.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -24,6 +27,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -105,7 +109,15 @@ fun NewsRow(
     news: News,
     modifier: Modifier = Modifier
 ) {
-    Card(modifier = modifier) {
+    val context = LocalContext.current
+    Card(
+        modifier = modifier
+            .clickable {
+                context.startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse(news.url))
+                )
+            }
+    ) {
         Row(
             modifier = modifier
                 .padding(dimensionResource(R.dimen.dp_8))

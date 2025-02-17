22af15981c69a8edc980c138ff77c89ca9e0be9c
roshan <roshan.shinde164@gmail.com>
2025-02-17 23:35:56 +0530

    added DataStore dependency to store user preferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.converter.moshi)
     implementation(libs.moshi.kotlin)
     implementation(libs.coil.compose)
+    implementation(libs.androidx.datastore.preferences)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
